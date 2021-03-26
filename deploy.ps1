6a87d1a07bf73287967667ea5f27c5958aaefba8
Thomas De Pauw <thdepauw@hotmail.com>
2021-03-26 22:11:53 +0100

    fix deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -7,5 +7,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:thdepauw/devops-changelog.git master:gh-pages
+git push -f https://github.com/thdepauw/devops-changelog.git master:gh-pages
 cd ..
