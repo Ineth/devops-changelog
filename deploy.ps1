3b602afcc6e6792a3b017464107e46e3bd47db97
Thomas De Pauw <thdepauw@hotmail.com>
2021-03-26 22:09:59 +0100

    Convert deploy to powershell

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,3 @@
-set -e
 # build
 npm run build
 # navigate into the build output directory
@@ -9,4 +8,4 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:thdepauw/devops-changelog.git master:gh-pages
-cd -
+cd ..
